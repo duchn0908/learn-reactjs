5cb10a74f4fb5cd40364448fbb7e815408c7b62e
Duchn <duchn.0908@gmail.com>
2024-01-11 17:28:03 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,51 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Hoang Duc';
+  const age = 18;
+  const ismale = true;
+  const student = {
+    name: 'Easy Frontend'
+  };
+  const colorList = ['red', 'green', 'blue', 'yellow']
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hoang nguyen
+          Hoang Nguyen Duc
         </p>
+        <p>Xin chao {name} - {age} {ismale ? 'Male' : 'Female'}</p>
 
+        {ismale ? <p>Minh la nam</p> : <p>Minh la nu</p>}
+
+        {ismale && <p>Minh la con trai</p>}
+        {!ismale && <p>Minh la nu</p>}
+
+
+        {ismale && (
+          <div>
+            <p>Minh la con trai</p>
+            <p>Minh la con trai</p>
+            <p>Minh la con trai</p>
+          </div>)
+        }
+        {ismale && (
+          <>
+            <p>Minh la con trai 1</p>
+            <p>Minh la con trai 2</p>
+            <p>Minh la con trai 3</p>
+          </>)
+        }
+        <p>{student.name}</p>
+        <ul>
+          {colorList.map(color => (
+            <li style={{ color }}>{color}</li>
+          )
+
+          )}
+        </ul>
       </header>
     </div>
   );

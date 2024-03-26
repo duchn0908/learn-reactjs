07ff9dd2456a96fac4c8bd45ce184ea9cc2714f7
Duchn <duchn.0908@gmail.com>
2024-03-26 16:30:57 +0700

    sfsaf

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,9 +1,15 @@
+
+import logo from './logo.jpeg.webp';
 const Header = () => {
     return (
         <div className="header-top">
             <div className="container">
                 <div className="row">
-                    <div className="col-6">Trai</div>
+                    <div className="col-6">
+                        <a href="#">
+                            <img src={logo}></img>
+                        </a>
+                    </div>
                     <div className="col-6">Phai</div>
                 </div>
             </div>
